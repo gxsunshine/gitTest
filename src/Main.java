4c456aed8ed7e294a342745828c6761954be6cb1
gx <375870381@qq.com>
2020-04-29 15:13:44 +0800

    second modify main.java

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("modify main.java");
+        System.out.println("second modify main.java");
     }
 }
