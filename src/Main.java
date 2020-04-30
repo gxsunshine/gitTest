615cd7c292dd63ab2a937046330fdad0c0a0ac66
gxsunshine <58652966+gxsunshine@users.noreply.github.com>
2020-04-30 10:09:32 +0800

    second remote modify
    second remote modify

--- src/Main.java
+++ src/Main.java
@@ -10,5 +10,6 @@ public class Main {
         System.out.println("sixth modify main.java");
         System.out.println("local modify");
         System.out.println("remote modify");
+        System.out.println("second remote modify");
     }
 }
