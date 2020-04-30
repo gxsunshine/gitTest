b92ec60c37a1c5dcc68169aba0d188dd75e32f55
gx <375870381@qq.com>
2020-04-30 10:33:05 +0800

    clear main.java

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        System.out.println("modify main.java");
-        System.out.println("second modify main.java");
-        System.out.println("third modify main.java");
-        System.out.println("fourth modify main.java");
-        System.out.println("fifth modify main.java");
-        System.out.println("sixth modify main.java");
-        System.out.println("local modify");
-        System.out.println("remote modify");
-        System.out.println("second remote modify");
-        System.out.println("second local modify");
-        System.out.println("second remote modify");
+
     }
 }
