99abe191a6b2038e0cde4a75e7083690483da9dc
gx <375870381@qq.com>
2020-04-30 10:53:22 +0800

    second local modify

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("first local modify");
         System.out.println("fisrt remote modify");
+        System.out.println("second local modify");
         System.out.println("second remote modify");
     }
 }
